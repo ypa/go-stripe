e7785d4e1c06e2e95bcb2979a2fb0d63ac84c0b0
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-15 20:10:38 -0800

    Refactor hardcoded routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,13 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+
+	stringMap := make(map[string]string)
+	stringMap["publishable_key"] = app.config.stripe.key
+
+	if err := app.renderTemplate(w, r, "terminal", &templateData{
+		StringMap: stringMap,
+	}); err != nil {
 		app.errorLog.Println(err)
 	}
 }
