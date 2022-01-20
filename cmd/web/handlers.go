08c504745fd0d8cb3be78cdf1c8ceb5fd637f400
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-19 19:44:18 -0800

    Add Charge once product page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -43,3 +43,10 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 		app.errorLog.Println(err)
 	}
 }
+
+// ChargeOnce displays the page to one widget
+func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "buy-once", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+}
