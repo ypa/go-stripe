4e9e110b19ef3cb51c346f8fdc1a314928a9bbb5
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-21 19:58:59 -0800

    Get Stripe Publishable key from template data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,12 +8,7 @@ import (
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 
-	stringMap := make(map[string]string)
-	stringMap["publishable_key"] = app.config.stripe.key
-
-	if err := app.renderTemplate(w, r, "terminal", &templateData{
-		StringMap: stringMap,
-	}, "stripe-js"); err != nil {
+	if err := app.renderTemplate(w, r, "terminal", &templateData{}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
 }
