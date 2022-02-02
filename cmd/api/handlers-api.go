cc62560db08e457ee12a5b0f83cf337370f26145
Yan P Aung <yan.pye.aung@gmail.com>
2022-02-01 20:03:22 -0800

    Update create customer and subscribe handler with card method calls

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -110,8 +110,28 @@ func (app *application) CreateCustomerAndSubscribeToPlan(w http.ResponseWriter, 
 
 	app.infoLog.Println(data.Email, data.LastFour, data.PaymentMethod, data.Plan)
 
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: data.Currency,
+	}
+
+	stripeCustomer, msg, err := card.CreateCustomer(data.PaymentMethod, data.Email)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	subscriptionID, err := card.SubscribeToPlan(stripeCustomer, data.Plan, data.Email, data.LastFour, "")
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	app.infoLog.Println("subscription id is", subscriptionID)
+
 	okay := true
-	msg := ""
+	// msg := ""
 
 	resp := jsonResponse{
 		OK:      okay,
